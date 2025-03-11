02ce45bbe41827504d97228ffcfdc134fc90eb4b
ARagulan <aswiniragulan@gmail.com>
2025-03-11 17:51:46 -0400

    Update redalliance.py

---
+++ Worbots/redalliance.py
@@ -0,0 +1,11 @@
+from machine import Pin
+import neopixel
+import time
+
+np = neopixel.NeoPixel(Pin(28), 156)
+
+while True:
+    for i in range(156): 
+        difference = j-i
+        if difference > 1:
+            np[j]: (255,0, 0)
